88f32bac3bc45e4160e1f20b14efa8624705b993
James Sinkler <jamessinkler713@gmail.com>
2021-01-31 20:14:56 -0800

    adds some meta description to pages

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,10 @@ import { Helmet } from 'react-helmet'
 export const wrapRootElement = ({ element }) => {
   return (
     <OrderProvider>
-      <Helmet title="WDArt" defer={false} />
+      <Helmet>
+        <title>WDArt</title>
+        <meta name="description" content="High end wall art. The best photography on your wall." />
+      </Helmet>
       {element}
     </OrderProvider>
   )
