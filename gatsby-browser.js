9d34a46ba5238c1d1873c6ba69f83dc356d6fca7
James Sinkler <jamessinkler713@gmail.com>
2021-01-31 19:46:22 -0800

    adds helmet with title in wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { OrderProvider } from './src/components/OrderContext'
 //this should look pretty similar to gatsby-browser page
+import { Helmet } from 'react-helmet'
 
 export const wrapRootElement = ({ element }) => {
   return (
     <OrderProvider>
+      <Helmet title="WDArt" defer={false} />
       {element}
     </OrderProvider>
   )
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { OrderProvider } from './src/components/OrderContext'
+import { Helmet } from 'react-helmet'
 //this should look pretty similar to gatsby-browser page
 
 export const wrapRootElement = ({ element }) => {
   return (
     <OrderProvider>
+      <Helmet title="WDArt" defer={false} />
       {element}
     </OrderProvider>
   )
