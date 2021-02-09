e5698fff36b7eeb8473862d1bf5b63aa45eed4d4
James Sinkler <jamessinkler713@gmail.com>
2021-02-08 20:02:19 -0800

    pull in art data to main wallArt page

--- src/pages/wallArt.js
+++ src/pages/wallArt.js
@@ -1,15 +1,62 @@
 import React, {useState, useEffect, useContext} from 'react'; 
 import {OrderContext} from '../components/OrderContext'
+import Img from 'gatsby-image'
+import { graphql, Link } from 'gatsby';
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+
+const ArtGrid = styled.div`
+  display: grid;
+  gap: 3rem;
+  grid-template-columns: repeat(3, 200px)
+`;
  
-const WallArt = ()=> { 
+const WallArt = ({ data })=> { 
   const [order, setOrder] = useContext(OrderContext)
+  console.log(data)
+  const artArray = data.art.nodes.map((node)=> {
+    return (
+      <div>
+        <p>{node.title}</p>
+        <Link to={node.slug.current}>{node.title}</Link>
+        <button onClick={()=> setOrder([ ...order, node ])}>Add to Cart</button>
+        <Img fluid={node.wallArtImg.asset.fluid} alt={node.title} />
+      </div>
+  )})
   return ( 
      <div> 
        <p>CartCount: {order.length}</p>
-       <button onClick={()=>setOrder( [ ...order, 'newArt'])}>
-         Add to order
-       </button>
+       <ArtGrid>
+         {artArray}
+       </ArtGrid>
      </div> 
   ) 
 } 
+
+export const query = graphql`
+  query AllArt {
+    art: allSanityWallArt {
+      nodes {
+        title
+        slug {
+          current
+        }
+        credit
+        category{
+          title
+          description
+        }
+        wallArtImg {
+          asset {
+            fluid(maxWidth:200) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
 export default WallArt;
