f61c9aa5e6b0e3301d0ad7b2a60f4af114350fb4
James Sinkler <jamessinkler713@gmail.com>
2021-06-09 13:33:59 -0700

    create success page, like homepage but with success message

---
+++ src/pages/success.js
@@ -0,0 +1,79 @@
+import * as React from "react";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import Layout from "../components/layout";
+import {Link} from 'gatsby';
+
+// trying a different landing page
+import background from "../images/mountain.jpg";
+
+const Container = styled.div`
+  background: url(${background}) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  min-width: 100%;
+  height: 80vh;
+  /* header takes up 7vh */
+  /* footer will take the other 10vh */
+`;
+
+const CatchPhrase = styled.div`
+  display: -webkit-flex; /* safari */
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.1);
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-family: "Mulish", sans-serif;
+`;
+
+const divStyles = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const h1Styles = css`
+  margin: 0 auto;
+  font-weight: 300;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+const h1BoldStyles = css`
+  margin: 0 auto;
+  font-weight: 700;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`;
+
+const Home = () => {
+  return (
+    <Layout>
+      <Container>
+        <CatchPhrase>
+          <div styles={divStyles}>
+            <div>
+              <h1 css={h1Styles}>Success, you just placed an order</h1>
+            </div>
+            <div css={{ textAlign: "center" }}>
+              <h1 css={h1BoldStyles}>It'll be on your wall soon!</h1>
+            </div>
+            <div css={{ textAlign: "center" }}>
+              <Link css={{ color: "white" }}to='/'>Back to home</Link>
+            </div>
+          </div>
+        </CatchPhrase>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Home;
