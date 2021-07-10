a4d5c6f8516227f16c804157d2c5ef47eb17237c
James Sinkler <jamessinkler713@gmail.com>
2021-07-10 17:02:31 -0600

    set up hamburger for small screen sizes w/alt nav dropdown

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { OrderContext } from "../components/OrderContext";
 import { Link } from "gatsby";
 import styled from "@emotion/styled";
 import cart from "../images/cart.svg";
+import { GiHamburgerMenu as Ham } from "react-icons/gi";
 
 const Container = styled.div`
   padding: 0 1rem;
@@ -86,6 +87,66 @@ const Img = styled.img`
   width: 30px;
   height: 30px;
 `;
+const LinkContainer = styled.div`
+  display: flex;
+  @media (max-width: 550px) {
+    display: none;
+  }
+`;
+const LowerLinkContainer = styled.div`
+position: absolute;
+margin-top: 14vh;
+height: 7vh;
+background: black;
+display: {({hidden}) => (hidden ? 'none' : 'block');
+`;
+const LowerStyledLink = styled(Link)`
+  display: inline-block;
+  color: #f4a15d;
+  text-decoration: none;
+  margin: 0 1.2rem;
+  position: relative;
+
+  :after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 0%;
+    /* adds a line transition upon hover */
+    content: ".";
+    color: transparent;
+    /* background: #000; */
+    background: #fff;
+    height: 1px;
+    transition: all 0.3s ease-in;
+  }
+
+  :hover {
+    /* color: #000; */
+    color: #fff;
+    ::after {
+      width: 100%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    padding: 10px 0;
+    font-size: 1rem;
+    z-index: 6;
+  }
+`;
+
+const MyHamburger = styled(Ham)`
+  display: none;
+  @media (max-width: 550px) {
+    display: block;
+  }
+  color: #f4a15d;
+  width: 32px;
+  height: 32px;
+`;
+
 const NumberItems = styled.div`
   position: absolute;
   align-items: center;
@@ -105,6 +166,13 @@ const NumberItems = styled.div`
 
 const Nav = () => {
   const [order] = useContext(OrderContext);
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const handleNavigation = (path)=> {
+  // reset dropDown
+  // handle navigation
+
+  }
 
   return (
     <Container>
@@ -112,11 +180,20 @@ const Nav = () => {
         WDArt
       </LogoWrapper>
 
-      <StyledLink to="/">Home</StyledLink>
-      <StyledLink to="/about/">About</StyledLink>
-      {/* for now, no blogs... */}
-      {/* <StyledLink to='/blogs'>Blogs</StyledLink> */}
-      <StyledLink to="/wallArt/">Products</StyledLink>
+      <MyHamburger onClick={() => setShowDropDown(!showDropDown)} />
+      <LinkContainer>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/about/">About</StyledLink>
+        {/* for now, no blogs... */}
+        {/* <StyledLink to='/blogs'>Blogs</StyledLink> */}
+        <StyledLink to="/wallArt/">Products</StyledLink>
+      </LinkContainer>
+      <LowerLinkContainer hidden={showDropDown}>
+        <LowerStyledLink onClick={()=>setShowDropDown(!showDropDown)} to="/">Home</LowerStyledLink>
+        <LowerStyledLink to="/about/">About</LowerStyledLink>
+        <LowerStyledLink to="/wallArt/">Products</LowerStyledLink>
+      </LowerLinkContainer>
+
       <StyledLink to="/cart/">
         <Img src={cart} alt="cart" />
       </StyledLink>
